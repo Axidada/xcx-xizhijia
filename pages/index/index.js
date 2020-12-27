63b75ce32ecbbe84275dea3bdfbe506a631c4bcf
yanxias <1213036993@qq.com>
2020-12-28 00:00:20 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,91 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperImg:[
+      { src:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572362098397&di=ce77f99622b7346244bb6b4357b1fcd9&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201806%2F28%2F20180628204542_qojqt.jpg"},
+      { src:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572362030300&di=f6620e772fd1b2a843b321d7dbdfaa02&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201801%2F26%2F20180126005333_T835x.jpeg"},
+      { src:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572362120836&di=3796e23a2b5c840eab15c6a2b4f58509&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201809%2F22%2F20180922230455_VZhYu.thumb.700_0.jpeg"}
+    ],
+    newsList: [
+      {
+        id: '356412',
+        title: '鞠婧祎，1994年6月18日出生于四川省遂宁市，中国内地流行乐女歌手、影视演员。',
+        poster: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572362098397&di=ce77f99622b7346244bb6b4357b1fcd9&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201806%2F28%2F20180628204542_qojqt.jpg',
+        add_date: '2019 - 10 - 01'
+      },
+      {
+        id: '546734',
+        title: '1994年6月18日，鞠婧祎出生于四川遂宁，高中就读于四川音乐学院附属中学。',
+        poster: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572362030300&di=f6620e772fd1b2a843b321d7dbdfaa02&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201801%2F26%2F20180126005333_T835x.jpeg',
+        add_date: "2019 - 10 - 05"
+      },
+      {
+        id: '239875',
+        title: "2013年8月，19岁的鞠婧祎通过层层选拔，从47976名参选者中脱颖而出，成为大型女子偶像团体SNH48的35名二期生成员之一。 ",
+        poster: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572362120836&di=3796e23a2b5c840eab15c6a2b4f58509&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201809%2F22%2F20180922230455_VZhYu.thumb.700_0.jpeg",
+        add_date: "2019 - 10 - 01"
+      }
+
+    ]
+  },
+  
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/my/index.js
@@ -0,0 +1,75 @@
+// pages/my/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nickName:"未登录",
+    src:"/images/login.png",
+    newsList: [
+      {
+        id: '356412',
+        title: '鞠婧祎，1994年6月18日出生于四川省遂宁市，中国内地流行乐女歌手、影视演员。',
+        poster: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572362098397&di=ce77f99622b7346244bb6b4357b1fcd9&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201806%2F28%2F20180628204542_qojqt.jpg',
+        add_date: '2019 - 10 - 01'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
